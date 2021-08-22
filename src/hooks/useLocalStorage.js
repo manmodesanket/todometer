3c4dc388d2bbfcf4a4fd7b7d60419c2ae909e922
manmodesanket <manmodesanket@gmail.com>
2021-08-22 22:10:56 +0530

    useLocalStorage implemented and used

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -7,31 +7,19 @@ export default function useLocalStorage(key, initialValue) {
 
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      // Get from local storage by key
       const item = localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      // If error also return initialValue
-      console.log(error);
       return initialValue;
     }
   });
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
   const setValue = (value) => {
     try {
-      // Allow value to be a function so we have same API as useState
       const valueToStore =
         value instanceof Function ? value(storedValue) : value;
-      // Save state
       setStoredValue(valueToStore);
-      // Save to local storage
       localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error);
-    }
+    } catch (error) {}
   };
   return [storedValue, setValue];
 }
